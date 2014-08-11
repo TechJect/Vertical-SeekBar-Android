1a2217c5b9ffd596347dcfe7f16a58dec7d7a915
Julian Knight <charles@rabidaudio.com>
2014-08-11 13:31:02 -0400

    Added listener, some refactor

--- sample/src/android/widget/VerticalSeekBar.java
+++ sample/src/android/widget/VerticalSeekBar.java
@@ -3,55 +3,75 @@ package android.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
+import android.widget.SeekBar;
+
+import org.jetbrains.annotations.NotNull;
 
 public class VerticalSeekBar extends SeekBar {
+
+    private OnSeekBarChangeListener myListener;
 
     public VerticalSeekBar(Context context) {
         super(context);
     }
-
+    public VerticalSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
     public VerticalSeekBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
-    public VerticalSeekBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(h, w, oldh, oldw);
+        super.onSizeChanged(h, w, oldh, oldw); //switch width, height
     }
 
     @Override
     protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec); //switch width, height
         setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
     }
 
-    protected void onDraw(Canvas c) {
+    @Override
+    public void setOnSeekBarChangeListener(OnSeekBarChangeListener mListener){
+        this.myListener = mListener;
+    }
+
+    protected void onDraw(@NotNull Canvas c) {
         c.rotate(-90);
-        c.translate(-getHeight(),0);
+        c.translate(-getHeight(), 0);
 
         super.onDraw(c);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    public boolean onTouchEvent(@NotNull MotionEvent event) {
         if (!isEnabled()) {
             return false;
         }
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if(myListener != null)
+                    myListener.onStartTrackingTouch(this);
+                break;
             case MotionEvent.ACTION_MOVE:
-            case MotionEvent.ACTION_UP:
-            	int i=0;
-            	i=getMax() - (int) (getMax() * event.getY() / getHeight());
-                setProgress(i);
-                Log.i("Progress",getProgress()+"");
+                int max = getMax();
+                float eventY = event.getY();
+                int height = getHeight();
+                int calculation = getMax() - (int) (getMax() * event.getY() / getHeight());
+                if(calculation > max)
+                    calculation = max;
+                else if(calculation < 0)
+                    calculation = 0;
+                setProgress(calculation);
                 onSizeChanged(getWidth(), getHeight(), 0, 0);
+                if(myListener != null)
+                    myListener.onProgressChanged(this,calculation, false);
+                break;
+            case MotionEvent.ACTION_UP:
+                if(myListener != null)
+                    myListener.onStopTrackingTouch(this);
                 break;
 
             case MotionEvent.ACTION_CANCEL:
@@ -59,5 +79,4 @@ public class VerticalSeekBar extends SeekBar {
         }
         return true;
     }
-    
 }
